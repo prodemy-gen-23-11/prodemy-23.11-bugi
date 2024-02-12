c7d64b7c82c0a83c025ec74f6f2c389a53e0cb5e
Bugi Pradana Nugroho <buginugroho17@gmail.com>
2024-02-13 00:58:23 +0700

    add JS axios and rest api exercise

---
+++ gen-19-rest-api/dummy.js
@@ -0,0 +1,17 @@
+axios
+    .get("https://dummyjson.com/products")
+    .then((response) => {
+        console.log("products:", response.data.products);
+    })
+    .catch((error) => console.log(error));
+
+async function getUsers() {
+    try {
+        const response = await axios.get("https://dummyjson.com/users");
+        console.log("users:", response.data.users);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getUsers();
