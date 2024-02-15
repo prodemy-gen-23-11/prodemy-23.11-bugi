5c2948e8da1696d12d2c1f5ef85c6b24cd81e533
Bugi Pradana Nugroho <buginugroho17@gmail.com>
2024-02-16 01:57:54 +0700

    add admin page w/tanstack

---
+++ gen-19-react-admin/src/components/ProductCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ProductCard({productData, onClick}) {
+
+    return (
+        <div className="font-gill w-72 inline-block relative my-4" onClick={onClick}>
+            <div className="bg-gray-200 h-72 rounded-2xl">
+                <img src={productData.imageUrl} alt="product image"
+                    className="h-full w-full hover:scale-105 duration-300" />
+            </div>
+            <div class="flex justify-between items-start">
+                <h3 className="mt-4 text-xl font-bold">{productData.name}</h3>
+                <p className="mt-4 text-lg">{productData.price}</p>
+            </div>
+            <p className="mt-2">{productData.desc}</p>
+            <p className="mt-1">★★★★★ (121)</p>
+            <button className="py-2 px-4 mt-2 text-zinc-200 bg-gray-600 rounded-full hover:text-zinc-600 hover:bg-gray-300 hover:scale-105 duration-300">Add to Cart</button>
+        </div>
+    );
+}
+
+export default ProductCard;
---
+++ gen-19-react-admin/src/components/TanStackTable.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import useSWR from "swr";
+import axios from "axios";
+import {
+    useReactTable,
+    flexRender,
+    createColumnHelper,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel
+} from "@tanstack/react-table";
+
+function TanStackTable() {
+    const getData = (url) => axios.get(url).then((response) => response.data);
+
+    const { data, error, isLoading } = useSWR("http://localhost:3000/products", getData);
+
+    const [products, setData] = useState(data);
+
+    const changeData = (newData) => {
+        setData(newData);
+    };
+
+    const columnHelper = createColumnHelper();
+
+    const columns = [
+        columnHelper.accessor("id", {
+            cell: (info) => <span>{info.getValue()}</span>,
+            header: "ID"
+        }),
+        columnHelper.accessor("imageUrl", {
+            cell: (info) => <img
+                className="w-32 h-32"
+                src={info.getValue()} />,
+            header: "Image"
+        }),
+        columnHelper.accessor("name", {
+            cell: (info) => <span>{info.getValue()}</span>,
+            header: "Name"
+        }),
+        columnHelper.accessor("desc", {
+            cell: (info) => <span>{info.getValue()}</span>,
+            header: "Description"
+        }),
+        columnHelper.accessor("price", {
+            cell: (info) => <span>{info.getValue()}</span>,
+            header: "Current Price"
+        }),
+        columnHelper.accessor("priceMax", {
+            cell: (info) => <span>{info.getValue()}</span>,
+            header: "Normal Price"
+        }),
+        ,
+        columnHelper.accessor("", {
+
+            header: "Action"
+        })
+    ]
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel()
+    });
+
+    if (!isLoading) return (
+        <div className="mx-auto">
+            <table className="border border-gray-600 w-full text-left">
+                <thead className="bg-gray-400 text-zinc-50 text-center">
+                    {
+                        table.getHeaderGroups().map((headerGroup) => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <th key={header.id} className="px-2 border-x border-b border-gray-600">
+                                        {flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))
+                    }
+                </thead>
+
+                <tbody>
+                    {
+                        table.getRowModel().rows.length ? (
+                            table.getRowModel().rows.map((row, i) => (
+                                <tr
+                                    key={row.id}
+                                    className={`
+                                        ${i % 2 === 0 ? "" : "bg-gray-200"}
+                                    `}
+                                >
+                                    {/* {console.log(row)} */}
+                                    {row.getVisibleCells().map((cell) => (
+                                        <td key={cell.id} className="px-2 border-x border-gray-600">
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext())}
+                                            {console.log(cell)}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))
+                        ) : null
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default TanStackTable;
